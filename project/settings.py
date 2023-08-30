d7a71ce7764c68dee073cea0fde61d79cc789838
Shayan M Hussain <shayanhussain48@gmail.com>
2023-08-31 02:35:23 +0500

    redis cloud implementation

--- project/settings.py
+++ project/settings.py
@@ -96,9 +96,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            # "hosts": [(os.environ.get("REDIS_URL", "127.0.0.1"), 6379)],
-            "hosts": [(os.environ.get("REDIS_URL", "127.0.0.1"), 6379)],
-            # "hosts": [(os.environ.get("REDIS_URL", "rehansathio.pythonanywhere.com"), 6379)],
+            "hosts": [("redis://default:CKOFxBomrHKWCnqcLLIvc0RePltiJoA2@redis-15811.c281.us-east-1-2.ec2.cloud.redislabs.com:15811")],
         },
     },
 }
