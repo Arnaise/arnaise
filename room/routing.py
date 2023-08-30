6912a20b73080929fcf06ec95dbd2f4463d27992
Muhammad Rehan <75864401+MuhammadRehanRasool@users.noreply.github.com>
2023-08-31 01:45:01 +0500

    update

--- room/routing.py
+++ room/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'wss/room/(?P<room_code>\w+)/$', consumers.LobbyConsumer.as_asgi()),
+    re_path(r'ws/room/(?P<room_code>\w+)/$', consumers.LobbyConsumer.as_asgi()),
 ]
