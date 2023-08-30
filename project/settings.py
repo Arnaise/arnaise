ef6a3337c4108417a85c3786a1bf96243649d79c
Muhammad Rehan <75864401+MuhammadRehanRasool@users.noreply.github.com>
2023-08-31 01:12:01 +0500

    update

--- project/settings.py
+++ project/settings.py
@@ -95,7 +95,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [(os.environ.get("REDIS_URL", "127.0.0.1"), 6379)],
+            "hosts": [(os.environ.get("REDIS_URL", "159.65.222.117"), 6379)],
         },
     },
 }
